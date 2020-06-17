621bad7508cad918ec545dbc4cfaf9870607f994
Henry Doan <henrydoan96@gmail.com>
2020-06-16 20:02:30 -0600

    create form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
 import Contacts from './components/contacts/Contacts';
+import ContactForm from './components/contacts/ContactForm';
 
 class App extends Component {
   state = { contacts: [
@@ -9,10 +10,24 @@ class App extends Component {
     { id: 3, firstName: 'Alex', phone: '123-125-1233'},
   ]}
 
+  getId = () => {
+    // NOTE We are just using this as a helper function for id's since we aren't using a db yet
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  };
+
+  addContact = (incomingContact) => {
+    const { contacts } = this.state
+    const newContact = { id: this.getId(), ...incomingContact }
+    this.setState({ contacts: [ newContact, ...contacts ]})
+  }
+
   render() {
     const { contacts } = this.state
     return(
       <div>
+        <ContactForm addContact={this.addContact} />
         <Header size="huge" color='blue' textAlign='center'>
           Contact list
         </Header>
